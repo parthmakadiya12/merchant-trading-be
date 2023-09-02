481ec5fb438392c0766fe681772b1c78f6d2eff7
Parth Makadiya <parthmakadiya12@gmail.com>
2023-09-02 18:02:00 +0200

    initial commit

---
+++ src/main/java/com/parthmakadiya/merchant/trading/merchanttrading/MerchantTradingApplication.java
@@ -0,0 +1,13 @@
+package com.parthmakadiya.merchant.trading.merchanttrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantTradingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parthmakadiya/merchant/trading/merchanttrading/MerchantTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parthmakadiya.merchant.trading.merchanttrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
